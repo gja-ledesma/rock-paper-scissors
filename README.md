09cd27d3ebc565c281deb562978ac157409606db
neo-ran <gjaledesma@outlook.com>
2022-06-14 12:20:55 +0800

    Add function game()

--- README.md
+++ README.md
@@ -1,11 +1,2 @@
 # rock-paper-scissors
-The third project task in The Odin Project.
-
-<!-- 
-Visit this [site]() to see output.
-
-By completing this particular project, I have demonstrated:
-
-- 
--
--->
+The third project task in The Odin Project. Visit this [site](https://neo-ran.github.io/rock-paper-scissors/) to see output.
--- Script/javascript.js
+++ Script/javascript.js
@@ -27,3 +27,34 @@ function playRound(playerSelection, computerSelection) {
       } 
     }
   }
+
+function game(){
+    const games = 5;
+    let playerPoints = 0;
+    let computerPoints = 0;
+  
+    for(let i = 0; i < games; i++) {
+      let playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
+  
+      function computerPlay() { return (["paper","scissors","rock"])[Math.random() * 3 | 0]; }
+      let computerSelection = computerPlay();
+  
+      let round = playRound(playerSelection, computerSelection);
+      if(round === "wins"){
+        playerPoints++;
+        console.log("You Win!");
+      } else if(round === "loses"){
+        computerPoints++;
+        console.log("You Lose!");
+      } else if(round === "ties") {
+        console.log("It's a tie!");
+      } 
+    }
+  
+    if(playerPoints > computerPoints){
+      console.log('');
+      console.log("You win! " + playerPoints + " beats " + computerPoints);
+    } else {
+      console.log("You lose! " + computerPoints + " beats " + playerPoints);
+    }
+}
